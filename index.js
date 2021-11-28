a93e8a70c70899b2db3ad9034f0f5f2eafe9af06
nghiemtv <nghiemtv@chainos.vn>
2021-11-28 20:22:10 +0700

    update-code

---
+++ index.js
@@ -0,0 +1,17 @@
+/*
+ * @Author: nghiemtv.com
+ */
+$("#login-button").click(function (event) {
+    var userName = document.getElementById("userName").value;
+    var pwd = document.getElementById("pwd").value;
+    if (userName == "minhphuong" && pwd == "123") {
+      event.preventDefault();
+      $("form").fadeOut(500);
+      $(".wrapper").addClass("form-success");
+      setTimeout(function () {
+        location.href = "BirthdayCake.html";
+      }, 2000);
+    } else {
+      alert("Wrong Password");
+    }
+  });
